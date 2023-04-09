fdd65d0fe4f8fe364a5bb94f8869be1c3de71e73
David Smith <dave@davidsilvasmith.com>
2023-04-08 22:23:24 -0400

    Questions and responses are saved. Enter submits a question.

--- client/src/partials/Chatbox.jsx
+++ client/src/partials/Chatbox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { AuthContext } from "../utils/AuthContext";
 import { getPersonalData, getSleepData, getAllSleepData } from "../utils/data";
 import { fetchResponse } from "../utils/ChatGPT";
@@ -13,18 +13,40 @@ function ChatBox() {
     const [sex, setSex] = useState("");
     const [email, setEmail] = useState("");
     const { accessToken } = useContext(AuthContext);
-
+    
     const [startDate, setStartDate] = useState("2023-03-01");
     const [endDate, setEndDate] = useState("2023-03-07");
     const [sleepData, setSleepData] = useState(null);
 
     const [input, setInput] = useState("");
-    const [response, setResponse] = useState(null);
-
+    const [userMessages, setUserMessages] = useState([]);
+    const [response, setResponse] = useState([]);
+    const chatContainerRef = useRef(null);
     const [ouraData, setOuraData] = useState(null);
 
+    function sendMessage() {
+        const timestamp = new Date().toISOString();
+        setUserMessages((prevMessages) => [...prevMessages, { text: input, timestamp, type: "user" }]);
+        getResponse();
+        setInput(""); // Clear the input after sending the message
+    }
 
     useEffect(() => {
+        if (email) {
+            const timestamp = new Date().toISOString();
+            setResponse((prevResponse) => [
+                ...prevResponse,
+                {
+                    text: `Welcome to Oura.ai, ${email}! How can I help you with your lifestyle, health, and sleep? Please ask me a question and I'll do my best to provide an answer.`,
+                    timestamp,
+                    type: "response",
+                },
+            ]);
+        }
+    }, [email]);
+    
+    useEffect(() => {
+        console.log('using effect for things changing');
 
         console.log("AccessToken in chatbox", accessToken)
 
@@ -37,6 +59,9 @@ function ChatBox() {
                 setAge(data.age);
                 setSex(data.biological_sex)
                 setEmail(data.email)
+                console.log('```````````````````');
+                console.log(data);
+                console.log('```````````````````');
             } catch (error) {
                 console.error(error);
             }
@@ -54,21 +79,33 @@ function ChatBox() {
         fetchSleep();
     }, [accessToken, startDate, endDate]);
 
+    useEffect(() => {
+        scrollToBottom();
+    }, [userMessages, response]);
+
     console.log("personalData", personalData);
     console.log("sleepData", sleepData);
-
-
 
     async function getResponse() {
         try {
             const ouraData = parseSleepData(sleepData);
-            console.log("sending data to fetchResponse",ouraData)
-            console.log("Sending data to fetchResponse", input)
-            const response = await fetchResponse(input,ouraData);
-            setResponse(response);
-            console.log("Response", response);
+            const responseText = await fetchResponse(input, ouraData);
+            const timestamp = new Date().toISOString();
+            setResponse((prevResponse) => [...prevResponse, { text: responseText, timestamp, type: "response" }]);
         } catch (error) {
             console.error(error);
+        }
+    }
+
+    function combineAndSortMessages() {
+        const combinedMessages = [...userMessages, ...response];
+        combinedMessages.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+        return combinedMessages;
+    }
+
+    function scrollToBottom() {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
         }
     }
 
@@ -149,25 +186,25 @@ function ChatBox() {
                         <div
                             className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4"
                         >
-                            <div className="relative ml-3 text-sm bg-white mt-10 py-2 px-4 shadow rounded-xl min-w-fit">
-                                <div className="flex flex-row items-center justify-between">
-                                    <div className="flex flex-row items-center justify-between">
-                                        <div>Welcome to Oura.ai, {email}! How can I help you with your lifestyle, health, and sleep? Please ask me a question and I'll do my best to provide an answer.</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-col h-full overflow-x-auto mb-4">
-                                <div className="flex flex-col h-full">
-                                    {response ? (<div className="relative ml-3 text-sm bg-white mt-10 py-2 px-4 shadow rounded-xl min-w-fit">
-                                        <div className="flex flex-row items-center justify-between">
-                                            <div className="flex flex-row items-center justify-between">
-                                                <div>{response}</div>
-                                            </div>
+                            <div className="flex flex-col h-full overflow-x-auto mb-4" ref={chatContainerRef}>
+                            <div className="flex flex-col h-full">
+                                    {combineAndSortMessages().map((item, index) => (
+                                        <div key={index}>
+                                            {item.type === "response" ? (
+                                                <div className="ml-3 text-sm bg-gray-200 mt-4 py-2 px-4 shadow rounded-xl min-w-fit">
+                                                    <div>
+                                                        <strong>Oura AI: </strong>
+                                                        {item.text}
+                                                    </div>
+                                                </div>
+                                            ) : (
+                                                <div className="relative ml-auto text-sm bg-indigo-200 mt-4 py-2 px-4 shadow rounded-xl min-w-fit">
+                                                    {item.text}
+                                                </div>
+                                            )}
                                         </div>
-                                    </div>) : (<div>{response}</div>)}
-
+                                    ))}
                                 </div>
-
                             </div>
                             <div
                                 className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
@@ -195,12 +232,18 @@ function ChatBox() {
                                 </div>
                                 <div className="flex-grow ml-4">
                                     <div className="relative w-full">
-                                        <input
-                                            onChange={(e) => setInput(e.target.value)}
-                                            type="text"
-                                            id="chat-input"
-                                            className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
-                                        />
+                                    <input
+                                        onChange={(e) => setInput(e.target.value)}
+                                        onKeyPress={(e) => {
+                                            if (e.key === "Enter") {
+                                                sendMessage();
+                                            }
+                                        }}
+                                        value={input}
+                                        type="text"
+                                        id="chat-input"
+                                        className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                                    />
                                         <button
                                             className="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
                                         >
@@ -223,10 +266,7 @@ function ChatBox() {
                                 </div>
                                 <div className="ml-4">
                                     <button
-                                        onClick={() => {
-                                            getResponse();
-                                            document.getElementById("chat-input").value = "";
-                                        }}
+                                        onClick={sendMessage}
                                         type="button"
                                         className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
                                     >
